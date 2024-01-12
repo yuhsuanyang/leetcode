de3cbbf3368c256775b53abd06bc71677a93e28b
yuhsuanyang <sb0953330882@gmail.com>
2024-01-12 16:06:07 +0800

    move files to correct directory

--- C++/largest_odd_number_in_string.cpp
+++ C++/largest_odd_number_in_string.cpp
@@ -1,12 +1,11 @@
 //no. 1903
 #include <string>
 using namespace std;
-
 class Solution {
-public:
+  public:
     string largestOddNumber(string num) {
       int largest_index;
-      for(int i = num.length() - 1; i >= 0; i--){
+      for(int i = num.length(); i >= 0; i--){
         if (int(num[i] - '0') % 2){
           largest_index = i;
           break;
